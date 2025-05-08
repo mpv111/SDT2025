5d9dce74e4e691371829adb542b46373a3678b21
MANUEL PERALTA <mpv111@inlumine.ual.es>
2025-05-08 13:02:06 +0200

    project and some code

---
+++ ProyectoSDT1/src/CODE/code1.java
@@ -0,0 +1,10 @@
+package CODE;
+
+public class code1 {
+    public static void main(String[] args) {
+        int[] tabla = {5, 3, 8, 1};
+        for (int n : tabla) {
+            System.out.println(n);
+        }
+    }
+}
