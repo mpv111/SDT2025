5fb40a0aec0a3138a473ee1efe6f3156f715a558
MANUEL PERALTA <mpv111@inlumine.ual.es>
2025-05-12 19:02:56 +0200

    Edit print line branchA

--- ProyectoSDT1/src/CODE/code1.java
+++ ProyectoSDT1/src/CODE/code1.java
@@ -22,7 +22,7 @@ public class code1 {
             }
         }
 
-        System.out.println("Ordenado:");
+        System.out.println("Ordenado branchA:");
         for (int n : tabla) {
             System.out.println(n);
         }
