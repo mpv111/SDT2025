0c1f34032df5be10dff9d21508cd9bd20de0f6b7
MANUEL PERALTA <mpv111@inlumine.ual.es>
2025-05-12 19:21:02 +0200

    fix

--- ProyectoSDT1/src/CODE/code1.java
+++ ProyectoSDT1/src/CODE/code1.java
@@ -22,7 +22,7 @@ public class code1 {
             }
         }
 
-        System.out.println("Ordenado good:");
+        System.out.println("Ordenado good1:");
 
         for (int n : tabla) {
             System.out.println(n);
